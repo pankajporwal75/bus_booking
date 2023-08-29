b1091810b6d8a688213785f2f42c183b318d9661
ajay-birla <Ajay.birla@gemsessence.com>
2023-08-29 11:32:02 +0530

    Revert "Pundit"

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-// function myfunc(){
-// $(document).ready(function(){
-//     $(".apb").click(function(){
-//         $.ajax({
-//             url: change_status_path,
-//             type: "GET"
-//         })
-//     })
-// })
-// }
-// window.onload = myfunc;
