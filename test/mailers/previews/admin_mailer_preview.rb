e1140344f0eb62a05ee83416741046ec539aa5a0
Pankaj Porwal <pankajporwal75@gmail.com>
2023-09-05 19:38:17 +0530

    mailer preview

--- test/mailers/previews/admin_mailer_preview.rb
+++ test/mailers/previews/admin_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/admin_mailer
 class AdminMailerPreview < ActionMailer::Preview
 
+  def approve_email
+    @bus = Bus.find(8)
+    AdminMailer.with(bus: @bus).approve_email
+  end
 end
