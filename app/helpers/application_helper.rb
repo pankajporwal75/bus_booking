000020d23aecce512be819e9716017e174355874
Pankaj Porwal <pankajporwal75@gmail.com>
2023-09-06 19:35:05 +0530

    active storage implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
     def formatted_time(time)
         time.strftime("%I:%M %p")
     end
+
+    def profile_image(user)
+        if user.profile_image.attached?
+            image_tag user.profile_image
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reservations, dependent: :destroy
+  has_one_attached :profile_image
   # has_secure_password
   def busowner?
     type == 'BusOwner'
