1e4f02e1c91c400e342a3e49f355270ffe28a448
Pankaj Porwal <pankajporwal75@gmail.com>
2023-10-04 16:28:17 +0530

    pagination on user pofile page to see its reservations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @reservations = current_user.reservations
+    @reservations = current_user.reservations.paginate(page: params[:page], per_page: 5)
   end
 end
