8288eff7a975d356229a2caf6c26c82ddf248578
Pankaj Porwal <pankajporwal75@gmail.com>
2023-10-05 19:56:10 +0530

    otp mailer specs, admin mailer specs, admin controller specs

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,10 +1,18 @@
 class AdminsController < ApplicationController
   before_action :require_admin
+
+  def index
+    @users = User.user
+    @bus_owners = BusOwner.all
+    @buses = Bus.upcoming
+  end
+
   def change_status
     authorize current_user, :is_admin?
     @bus = Bus.find(params[:id])
     if @bus.approved?
       @bus.update(status: :disapproved)
+      AdminMailer.disapprove_email(@bus).deliver_now
       @bus.reservations.delete_all
       respond_to do |format|
         format.html {redirect_to buses_path}
@@ -12,6 +20,7 @@ class AdminsController < ApplicationController
       end
     else
       @bus.update(status: :approved)
+      AdminMailer.approve_email(@bus).deliver_now
       respond_to do |format|
         format.html {redirect_to buses_path}
         format.js
